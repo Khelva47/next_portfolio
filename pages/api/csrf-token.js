1a896e6f861567d49c32433cbbafe254b650fd55
BED-COM-32-20 <kelvinmponda47@gmail.com>
2025-07-29 15:31:42 +0200

    Created a csrf-token

---
+++ pages/api/csrf-token.js
@@ -0,0 +1,11 @@
+// pages/api/csrf-token.js
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    res.setHeader("Set-Cookie", "csrfToken=" + require("crypto").randomBytes(16).toString("hex") + "; HttpOnly; Path=/; SameSite=Strict")
+    res.status(200).json({ csrfToken: req.cookies.csrfToken || require("crypto").randomBytes(16).toString("hex") })
+  } else {
+    res.status(405).json({ message: "Method not allowed" })
+  }
+}
