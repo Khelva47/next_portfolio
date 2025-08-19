cbbe196aa23fe821f56253a52c4f33639badf940
BED-COM-32-20 <kelvinmponda47@gmail.com>
2025-08-19 19:06:48 +0200

    Removed the dark-mode button

--- src/app/components/Layout/MobileHeader.js
+++ src/app/components/Layout/MobileHeader.js
@@ -14,15 +14,6 @@ const MobileHeader = ({ isMobileMenuOpen, setIsMobileMenuOpen, isDarkMode, setIs
           </button>
           <span className="font-semibold text-gray-900 dark:text-white">Kelvin Mponda</span>
         </div>
-
-        {/* Dark Mode Toggle */}
-        <button
-          onClick={() => setIsDarkMode(!isDarkMode)}
-          className="p-2 text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg transition-colors"
-          aria-label="Toggle dark mode"
-        >
-          {isDarkMode ? <Sun className="w-5 h-5" /> : <Moon className="w-5 h-5" />}
-        </button>
       </div>
     </div>
   )
